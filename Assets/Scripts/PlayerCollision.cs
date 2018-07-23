d8a7d2b785306868df89d7207ee403072a5e1dd0
davidcgong <davidcgong@gmail.com>
2018-07-22 22:24:12 -0400

    first commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	void OnCollisionEnter (Collision collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "Obstacle") {
+			Debug.Log ("We hit an obstacle.");
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Add a forward force
+		// rb.AddForce(0, 0, forwardForce * Time.deltaTime);	
+		if (Input.GetKey ("d")) 
+		{
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey ("a")) 
+		{
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey ("w")) 
+		{
+			rb.AddForce (0, 0, sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey ("s")) 
+		{
+			rb.AddForce (0, 0, -sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
+		}
+	}
+}
